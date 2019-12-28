6b7775177a2b2fa06ea42d6763ca553aed27504d
Dana McMullen <dtwillia@syr.edu>
2019-12-28 15:48:52 -0500

    added layout.js to hold shared sections

--- site/src/components/layout.js
+++ site/src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 1200 }}>
+  <div style={{ margin: `10 auto`, maxWidth: 1000 }}>
     {children}
   </div>
 )
