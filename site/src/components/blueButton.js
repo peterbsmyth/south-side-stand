a3d11d105c6e119305024cf88730307dcd0d8d8a
Dana McMullen <dtwillia@syr.edu>
2019-12-28 16:57:35 -0500

    Added blueButton.js to components

---
+++ site/src/components/blueButton.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default () => (
+  <Layout>
+    <h1>Blue button component. This will be just the button and not the text or link. Import to pages that require it. </h1>
+  </Layout>
+)
