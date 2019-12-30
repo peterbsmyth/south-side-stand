287e0981ea382d6cf5629f2b56a63d114b1908f9
Dana McMullen <dtwillia@syr.edu>
2019-12-29 23:37:00 -0500

    corrected file name and coded base file structure

--- site/src/pages/contact.js
+++ site/src/pages/contact.js
@@ -1,7 +1,21 @@
-import React from "react"
-import Layout from "../components/layout"
-export default () => (
-  <Layout>
-    <h1>This is the contact page.</h1>
-  </Layout>
-)
+import React from 'react';
+import Layout from '../components/layout';
+import Navigation from '../components/Navigation';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+
+class Contact extends React.Component{
+  render() { 
+    return (
+    <div>
+      <Layout />
+      <Navigation />
+      <Header />
+      <Footer />
+    </div>
+    )
+  }
+}
+
+export default Contact;
