be15f9184f98a3c9dc60396dd5d6dd6dcc217310
Dana McMullen <dtwillia@syr.edu>
2019-12-29 23:40:38 -0500

    corrected file name and coded base file structure

--- site/src/pages/fatherhood.js
+++ site/src/pages/fatherhood.js
@@ -1,7 +1,21 @@
-import React from "react"
-import Layout from "../components/layout"
-export default () => (
-  <Layout>
-    <h1>This is the fatherhood page.</h1>
-  </Layout>
-)
+import React from 'react';
+import Layout from '../components/layout';
+import Navigation from '../components/Navigation';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+
+class Fatherhood extends React.Component{
+  render() { 
+    return (
+    <div>
+      <Layout />
+      <Navigation />
+      <Header />
+      <Footer />
+    </div>
+    )
+  }
+}
+
+export default Fatherhood;
