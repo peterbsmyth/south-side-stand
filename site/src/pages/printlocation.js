a168f8571cf4dd40a4dbd16bc34bb7a07a5b3a48
Dana McMullen <dtwillia@syr.edu>
2020-01-03 13:06:57 -0500

    added html

--- site/src/pages/printlocation.js
+++ site/src/pages/printlocation.js
@@ -3,5 +3,352 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h1>This is the print location page.</h1>
+
+
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <title>Print Locations</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+
+    <!-- Put your general CSS file here -->
+    <link rel="stylesheet" href="css/general.css" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Anton|Francois+One|Oswald|Roboto:700&display=swap" rel="stylesheet">
+ 
+  </head>
+
+  <body>
+ 
+    <div class="container">
+  
+    <header class="row py-4">
+    
+      <div class="col-3">
+        <img src="images/header_footer/the-stand-logo-full-blue.png" alt="the stand logo" width="200" />
+      </div>
+    
+      <div class="col-6">
+      </div>
+  
+      <div class="col-3">
+        
+        <div class="row">
+          <div class="col px-0">
+            
+            <div class="input-group input-group-sm mb-3">
+              <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2">
+        
+              <div class="input-group-prepend">
+                 <span class="input-group-text" id="basic-addon2"><img src="images/header_footer/iconfinder_search.png" alt="the search logo" width="24" /></span>
+              </div>
+            
+            </div>
+
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col text-right px-0">
+            <button type="button" class="btn btn-primary btn-md px-3 px-md-5">Donate</button>
+          </div>
+        </div>
+
+
+      </div>
+ 
+     </header>
+
+
+<div class="row">
+  <div class="col px-0">
+
+  <nav class="navbar navbar-expand-lg navbar-light">
+  
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+      <ul class="navbar-nav mr-auto">
+      
+        <li class="nav-item dropdown">
+        
+          <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stories</a>
+        
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">News</a>
+            <a class="dropdown-item" href="#">Community</a>
+            <a class="dropdown-item" href="#">Environment</a>
+            <a class="dropdown-item" href="#">Business</a>
+            <a class="dropdown-item" href="#">School & Youth</a>
+            <a class="dropdown-item" href="#">Entertainment</a>
+            <a class="dropdown-item" href="#">Religion & Faith</a>
+            <a class="dropdown-item" href="#">Fatherhood</a>
+            <a class="dropdown-item" href="#">Southside Achiever</a>
+
+          
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Photo Walk</a>
+            <a class="dropdown-item" href="#">They Wear Blue</a>
+            <a class="dropdown-item" href="#">My Housing Matters</a>
+            <a class="dropdown-item" href="#">From Where We Stand</a>
+          </div>
+
+        </li>
+
+        <li class="nav-item dropdown">
+        
+           <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
+        
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">History</a>
+              <a class="dropdown-item" href="#">Print Copies</a>
+              <a class="dropdown-item" href="#">Flickr</a>
+              <a class="dropdown-item" href="#">FAQ</a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown">
+        
+           <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Opportunites</a>
+        
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Job Openings</a>
+              <a class="dropdown-item" href="#">Volunteer</a>
+              <a class="dropdown-item" href="#">Skills Training</a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown">
+        
+           <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Calendar</a>
+        
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Events</a>
+              <a class="dropdown-item" href="#">Announcements</a>
+            </div>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link px-3" href="#">Contact<span class="sr-only"></span></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link px-3" href="#">Getting Involved<span class="sr-only"></span></a>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+
+  </div>
+</div>
+
+
+<h1>Want a print copy?</h1>
+<h2>The Stand is distributed to the following locations:</h2>
+
+<div class="row py-3">
+  <div class="col-md-4">
+    <img src="images/printlocation/syr.JPEG" alt="map" class="img-fluid pt-2 pb-4" />
+      <h3>A</h3>
+        <p class="p-bold">AIDS Community Resource <span class="location">627 W. Genesee St.</span></p>
+        <p class="p-bold">Atonement Ministries <span class="location">116 W. Glen Ave</span></p>
+        
+      <h3>B</h3>
+        <p class="p-bold">Beauchamp Branch Library <span class="location"> 2111 S. Salina St.</span></p>
+        <p class="p-bold">Bethany Baptist Church <span class="location">149 Beattie St.</span></p>
+        <p class="p-bold">Betts Branch Library <span class="location">4862 S. Salina St.</span></p>
+        <p class="p-bold">Blue Brothers Barber Shop <span class="location">2036 S. Salina St.</span></p>
+        <p class="p-bold">Brady Faith Center <span class="location">404 South Ave.</span></p>
+        <p class="p-bold">Branch’s Driving School <span class="location">300 W. Genesee St.</span></p>
+        <p class="p-bold">Brighton Bakery <span class="location">446 E. Brighton Ave.</span></p>
+        <p class="p-bold">Brighton Towers <span class="location">821 East Brighton Ave.</span></p>
+        
+      <h3>C</h3>
+        <p class="p-bold">Cafe Kubal <span class="location">720 University Ave. & 401 S. Salina St.</span></p>
+        <p class="p-bold">Cannon Street Community Center<span class="location"> 529 Cannon St.</span></p>
+        <p class="p-bold">City Hall <span class="location">233 E. Washington St.</span></p>
+        <p class="p-bold">City Hall Commons <span class="location">201 East Washington Street.</span></p>
+        <p class="p-bold">City Wireless <span class="location">580 S. Salina St.</span></p>
+        <p class="p-bold">Cole Muffler <span class="location">2527 S. Salina St.</span></p>
+        <p class="p-bold">Colonial Laundromat <span class="location">3901 S. Salina St. & 1805 South Ave.</span></p>
+        <p class="p-bold">Community Folk Art Center <span class="location">805 E. Genesee St.</span></p>
+        <p class="p-bold">Corcoran High School <span class="location">919 Glenwood Ave.</span></p>
+        <p class="p-bold">CUSE Center <span class="location">115 E. Jefferson St., Suite 300</span></p>
+        <p class="p-bold">Cut Kings <span class="location">754 N Salina St.</span></p>
+    
+    
+        
+      
+
+    </div>
+
+    <div class="col-md-4">
+      <h3>D</h3>
+        <p class="p-bold">Deb’s Convenience <span class="location">2419 S. Salina St.</span></p>
+        <p class="p-bold">The Determination Center <span class="location">1654 W. Onondaga St.</span></p>
+        <p class="p-bold">Dr. King Magnet School <span class="location">416 E. Raynor Ave.</span></p>
+        <p class="p-bold">Dunbar Center <span class="location">1453 S. State St.</span></p>
+        <p class="p-bold">Dunk & Bright <span class="location">2648 S. Salina St.</span></p>
+        <p class="p-bold">Dunkin Donuts <span class="location">3915 S. Salina St.</span></p>
+      
+  
+      <h3>E</h3>
+        <p class="p-bold">Eagle Wings Academy <span class="location">423 Valley Dr.</span></p>
+        <p class="p-bold">Eastside FRC <span class="location">202 S. Beech St.</span></p>
+        <p class="p-bold">Expeditionary Learning Middle School <span class="location">4942 S. Salina St.</span></p>
+
+      <h3>F</h3>
+        <p class="p-bold">Factory Direct <span class="location">2711 S. Salina St.</span></p>
+        <p class="p-bold">Faith Hope Community Center <span class="location">1029 Montgomery St.</span></p>
+
+      <h3>G</h3>
+        <p class="p-bold">Green Hills Market <span class="location">5933 South Salina St.</span></p>
+
+      <h3>I</h3>
+        <p class="p-bold">ITC High School <span class="location">258 E Adams St.</span></p>
+
+      <h3>J</h3>
+        <p class="p-bold">Jackson Hewitt Tax Service <span class="location">4610 S. Salina St.</span></p>
+        <p class="p-bold">Jerk Hut Restaurant <span class="location">440 South Ave.</span></p>
+        <p class="p-bold">John’s Appointment Barber Shop <span class="location">4403 S. Salina St.</span></p>
+
+      <h3>M</h3>
+        <p class="p-bold">Mary Nelson Youth Center <span class="location">2849 S. Salina St.</span></p>
+        <p class="p-bold">McKinley-Brighton Magnet Elementary School <span class="location">141 W. Newell St.</span></p>
+        <p class="p-bold">Meals on Wheels <span class="location">300 Burt St #1</span></p>
+        <p class="p-bold">Media Unit <span class="location">327 Montgomery St.</span></p>
+        <p class="p-bold">Mr. H.R. Clothing <span class="location">2217 S. Salina St.</span></p>
+
+      <h3>N</h3>
+        <p class="p-bold">New Hope Missionary Baptist Church <span class="location">241 W. Lafayette Ave.</span></p>
+
+      <h3>O</h3>
+        <p class="p-bold">Onondaga County Public Library, The Galleries of Syracuse<span class="location"> 447 S. Salina St.</span></p>
+
+      <h3>P</h3>
+        <p class="p-bold">Payton Memorial Temple COGIC <span class="location">1816 Midland Ave.</span></p>
+        <p class="p-bold">PEACE Inc. South Side Center <span class="location">136 Dr. Martin Luther King West.</span></p>
+        <p class="p-bold">People’s A.M.E. Zion Church <span class="location">2306 S. Salina St.</span></p>
+
+      
+
+    </div>
+
+    <div class="col-md-4">
+      <h3>R</h3>
+        <p class="p-bold">Reformed Presbyterian Church <span class="location">2517 S. Salina St.</span></p>
+
+      <h3>S</h3>
+        <p class="p-bold">Salt City Coffee <span class="location">509 W. Onondaga St.</span></p>
+        <p class="p-bold">Salvation Army <span class="location">1704 S. Salina St.</span></p>
+        <p class="p-bold">Social service offices <span class="location">518 James St.</span></p>
+        <p class="p-bold">Southside Academy Charter School <span class="location">2200 Onondaga Creek Boulevard</span></p>
+        <p class="p-bold">South Side Communication Center <span class="location">2331 S. Salina St.</span></p>
+        <p class="p-bold">South Side Innovation Center <span class="location">2610 S. Salina St.</span></p>
+        <p class="p-bold">Southwest Community Center <span class="location">401 South Ave.</span></p>
+        <p class="p-bold">SUN <span class="location">1540 S Salina St.</span></p>
+        <p class="p-bold">Syracuse City School District Office <span class="location">725 Harrison St.</span></p>
+        <p class="p-bold">Syracuse Community Health Center 8<span class="location">19 S. Salina St.</span></p>
+        <p class="p-bold">Syracuse Housing Authority <span class="location">516 Burt St.</span></p>
+        <p class="p-bold">Syracuse Model Neighborhood <span class="location">1721 S. Salina St.</span></p>
+        <p class="p-bold">Syracuse Regional Transportation Center <span class="location">131 Alliance Bank Parkway</span></p>
+        <p class="p-bold">Syracuse Salt of the Earth Ministries <span class="location">320 W. Onondaga St.</span></p>
+        <p class="p-bold">Syracuse Stage <span class="location">820 E. Genesee St.</span></p>
+
+      <h3>T</h3>
+        <p class="p-bold">The Vision Center <span class="location">1221 S. Salina St.</span></p>
+        <p class="p-bold">The Warehouse <span class="location">350 W. Fayette St.</span></p>
+      <h3>W</h3>
+        <p class="p-bold">Wescott Community Center<span class="location"> 826 Euclid Ave.</span></p>
+        <p class="p-bold">Wilson Park Community Center <span class="location">1117 S. McBride St.</span></p>
+
+      <h3>Z</h3>
+        <p class="p-bold">Zen Center of Syracuse <span class="location">266 W. Seneca Turnpike</span></p>
+        
+      <br>
+      <img src="images/printlocation/newspaper.png" alt="newspaper" width="300px" class="img-fluid text-center" />
+      </div>
+
+      <div class="col-12 text-center pt-5 pb-4 px-5">
+          <h5>To add your business or send a suggestion for a distribution site, please call The Stand at (315) 882-1054 or e-mail ashley@mysouthsidestand.com.</h5>
+      </div>
+    </div>
+
+  
+
+  <footer class="row background-blue footer">
+    
+    <div class="col-md-3 py-4 text-center">
+      <img src="images/header_footer/the-stand-logo-white.png" alt="Stand Logo" width="150" />
+    </div>
+
+    <div class="col-md-6 py-4 text-center footer-text">
+      
+      <h4 class="pb-0">SUBSCRIBE</h4>
+      <p>To recieve a weekly newsletter with popular stories and upcoming events:</p>
+
+      <div class="input-group input-group-sm mb-3 container px-0">
+        
+        <input type="text" class="form-control" placeholder="Insert Email" aria-label="Insert Email" aria-describedby="basic-addon2">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon2">Submit</span>
+        </div>
+      
+      </div>
+
+      <ul class="list-inline">
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Stories</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">About</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Contact</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Getting Involved</a></li>
+      </ul>
+
+      <ul class="list-inline">
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Calendar</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Opportunities</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="https://www.flickr.com/photos/mysouthsidestand/" class="footer-link">Flickr</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Advertising</a></li>
+      </ul>
+            
+      <p class="footer-text">&copy; 2019 The South Side Stand. All rights reserved.</p>
+       
+    </div>
+
+
+    <div class="col-md-3 py-4 text-center">
+
+      <a target="new" href="https://www.facebook.com/TheStandNewspaper/" ><img src="images/header_footer/iconfinder_facebook.png" alt="Facebook Logo" width="36" class="mx-1"/></a>
+          
+      <a target="new" href="http://twitter.com/mysouthside" ><img src="images/header_footer/iconfinder_twitter.png" alt="Twitter Logo" width="36" class="mx-1" /></a>
+          
+      <a target="new" href="INSERT STAND INSTAGRAM LINK" ><img src="images/header_footer/iconfinder_instagram.png" alt="Instagram Logo" width="36" class="mx-1" /></a>
+    </div>
+
+</footer>
+  
+</div>
+  
+
+    <!-- Optional JavaScript but required for the cool stuff -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
   </Layout>
 )
