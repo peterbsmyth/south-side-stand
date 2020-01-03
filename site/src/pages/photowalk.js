cd69c236a3702b37afce7c21671ae4c333008159
Dana McMullen <dtwillia@syr.edu>
2020-01-03 13:04:28 -0500

    added html

--- site/src/pages/photowalk.js
+++ site/src/pages/photowalk.js
@@ -3,5 +3,323 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h1>This is the photowalk page.</h1>
+
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <title>Photo Walk</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+
+    <!-- Put your general CSS file here -->
+    <link rel="stylesheet" href="css/general.css" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Anton|Francois+One|Oswald|Roboto:700&display=swap" rel="stylesheet">
+ 
+  </head>
+
+  <body>
+ 
+    <div class="container">
+  
+    <header class="row py-4">
+    
+      <div class="col-3">
+        <img src="images/header_footer/the-stand-logo-full-blue.png" alt="the stand logo" width="200" />
+      </div>
+    
+      <div class="col-6">
+      </div>
+  
+      <div class="col-3">
+        
+        <div class="row">
+          <div class="col px-0">
+            
+            <div class="input-group input-group-sm mb-3">
+              <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2">
+        
+              <div class="input-group-prepend">
+                 <span class="input-group-text" id="basic-addon2"><img src="images/header_footer/iconfinder_search.png" alt="the search logo" width="24" /></span>
+              </div>
+            
+            </div>
+
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col text-right px-0">
+            <button type="button" class="btn btn-primary btn-md px-3 px-md-5">Donate</button>
+          </div>
+        </div>
+
+
+      </div>
+ 
+     </header>
+
+
+<div class="row">
+  <div class="col px-0">
+
+  <nav class="navbar navbar-expand-lg navbar-light">
+  
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+      <ul class="navbar-nav mr-auto">
+      
+        <li class="nav-item dropdown">
+        
+          <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stories</a>
+        
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">News</a>
+            <a class="dropdown-item" href="#">Community</a>
+            <a class="dropdown-item" href="#">Environment</a>
+            <a class="dropdown-item" href="#">Business</a>
+            <a class="dropdown-item" href="#">School & Youth</a>
+            <a class="dropdown-item" href="#">Entertainment</a>
+            <a class="dropdown-item" href="#">Religion & Faith</a>
+            <a class="dropdown-item" href="#">Fatherhood</a>
+            <a class="dropdown-item" href="#">Southside Achiever</a>
+
+          
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Photo Walk</a>
+            <a class="dropdown-item" href="#">They Wear Blue</a>
+            <a class="dropdown-item" href="#">My Housing Matters</a>
+            <a class="dropdown-item" href="#">From Where We Stand</a>
+          </div>
+
+        </li>
+
+        <li class="nav-item dropdown">
+        
+           <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
+        
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">History</a>
+              <a class="dropdown-item" href="#">Print Copies</a>
+              <a class="dropdown-item" href="#">Flickr</a>
+              <a class="dropdown-item" href="#">FAQ</a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown">
+        
+           <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Opportunites</a>
+        
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Job Openings</a>
+              <a class="dropdown-item" href="#">Volunteer</a>
+              <a class="dropdown-item" href="#">Skills Training</a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown">
+        
+           <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Calendar</a>
+        
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Events</a>
+              <a class="dropdown-item" href="#">Announcements</a>
+            </div>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link px-3" href="#">Contact<span class="sr-only"></span></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link px-3" href="#">Getting Involved<span class="sr-only"></span></a>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+
+  </div>
+</div>
+
+
+  <section>
+      
+        <div class="row">
+          <div class="col-md-12">
+            <h1>The 10th Annual Photo Walk</h1>
+          <div>
+        </div>
+
+         <div class="row">
+            <div class="col-md-5 px-3 py-1">
+              <img src="images/photowalk/poster.jpg" alt="Photo Walk Cover Page" class="img-fluid pb-3" />
+            </div>
+
+            <div class="col-md-7 py-1">
+              <p>The Stand will hold its annual Photo Walk for the 10th year Saturday, July 27.</p>
+              <p>A Photo Walk is a social photography event where photographers get together to explore a neighborhood, shoot photos and practice their skills.</p>
+              <p>Because this year is the 10th anniversary, multiple walks will be held. Interested participants may sign up for a specific walk or arrive day of the walk and pick which to join. </p>
+              <p>If interested in learning more, email ashley@mysouthsidestand.com or register online at <a href="https://docs.google.com/forms/d/e/1FAIpQLSdol4DBe9pV1-ilp-iWtcQVPjOAHSHgfjFalrabbQXTc4LtMA/closedform" target ="new">bit.ly/SouthSidePhotoWalk.</a></p>
+              <p><span class="location">The event will begin at 10 a.m. from the South Side Innovation Center, located at 2610 S. Salina St.</span></p>
+            </div>
+          </div>
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <h3 class="mb-4">Photo Walk Archive</h3>
+
+              <div class="row">
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157710050431582/" target="new"><img src="images/photowalk/2019.jpg" alt="Photowalk 2019" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2019</h2>
+                </div>
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157669687323687/" target="new"><img src="images/photowalk/2018.jpg" alt="Photowalk 2018" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2018</h2>
+                </div>
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157684050666613/" target="new"><img src="images/photowalk/2017.jpg" alt="Photowalk 2017" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2017</h2>
+                </div>
+
+              </div>
+
+              <div class="row">
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157670512552511/" target="new"><img src="images/photowalk/2016.jpg" alt="Photowalk 2016" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2016</h2>
+                </div>
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157653792318784/" target="new"><img src="images/photowalk/2015.jpg" alt="Photowalk 2015" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2015</h2>
+                </div>
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157646023682305/" target="new"><img src="images/photowalk/2014.jpg" alt="Photowalk 2014" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2014</h2>
+                </div>
+
+              </div>
+
+              <div class="row">
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157634624885794/" target="new"><img src="images/photowalk/2013.jpg" alt="Photowalk 2013" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2013</h2>
+                </div>
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157630684537012/" target="new"><img src="images/photowalk/2012.jpg" alt="Photowalk 2012" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2012</h2>
+                </div>
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157629847352129/" target="new"><img src="images/photowalk/2011.jpg" alt="Photowalk 2011" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2011</h2>
+                </div>
+
+              </div>
+
+               <div class="row">
+
+                <div class="col-md-4 pb-3">
+                  <a href="https://www.flickr.com/photos/mysouthsidestand/collections/72157624448596327/" target="new"><img src="images/photowalk/2010.jpg" alt="Photowalk 2010" class="img-fluid pb-2" /></a>
+                  <h2 class="mb-2">2010</h2>
+                </div>
+
+                <div class="col-md-4 pb-3">
+                </div>
+
+                <div class="col-md-4 pb-3">
+                </div>
+
+              </div>
+      
+ 
+
+  </section>
+  
+
+  <footer class="row background-blue footer">
+    
+    <div class="col-md-3 py-4 text-center">
+      <img src="images/header_footer/the-stand-logo-white.png" alt="Stand Logo" width="150" />
+    </div>
+
+    <div class="col-md-6 py-4 text-center footer-text">
+      
+      <h4 class="pb-0">SUBSCRIBE</h4>
+      <p>To recieve a weekly newsletter with popular stories and upcoming events:</p>
+
+      <div class="input-group input-group-sm mb-3 container px-0">
+        
+        <input type="text" class="form-control" placeholder="Insert Email" aria-label="Insert Email" aria-describedby="basic-addon2">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon2">Submit</span>
+        </div>
+      
+      </div>
+
+      <ul class="list-inline">
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Stories</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">About</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Contact</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Getting Involved</a></li>
+      </ul>
+
+      <ul class="list-inline">
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Calendar</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Opportunities</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="https://www.flickr.com/photos/mysouthsidestand/" class="footer-link">Flickr</a></li>
+
+        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Advertising</a></li>
+      </ul>
+            
+      <p class="footer-text">&copy; 2019 The South Side Stand. All rights reserved.</p>
+       
+    </div>
+
+
+    <div class="col-md-3 py-4 text-center">
+
+      <a target="new" href="https://www.facebook.com/TheStandNewspaper/" ><img src="images/header_footer/iconfinder_facebook.png" alt="Facebook Logo" width="36" class="mx-1"/></a>
+          
+      <a target="new" href="http://twitter.com/mysouthside" ><img src="images/header_footer/iconfinder_twitter.png" alt="Twitter Logo" width="36" class="mx-1" /></a>
+          
+      <a target="new" href="INSERT STAND INSTAGRAM LINK" ><img src="images/header_footer/iconfinder_instagram.png" alt="Instagram Logo" width="36" class="mx-1" /></a>
+    </div>
+
+</footer>
+  
+</div>
+  
+
+    <!-- Optional JavaScript but required for the cool stuff -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
   </Layout>
 )
