ccc8f7458223fa4b470e9e2905570ad8c78bcc91
Dana McMullen <danamcmullen@Danas-MBP.localdomain>
2020-01-08 20:43:25 -0500

    fixed html to render js file properly

--- site/src/pages/getinvolved.js
+++ site/src/pages/getinvolved.js
@@ -1,48 +1,34 @@
-import React from "react"
-import Layout from "../components/layout"
-export default () => (
-  <Layout>
-    <h1>This is the get involved page.</h1>
+import React, { Component } from 'react';
+// import logo from '../images/header_footer/the-stand-logo.png';
+// import '../styles/general.css';
 
-    <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+class Getinvolved extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" />
 
-    <title>Get Involved</title>
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
-
-    <!-- Put your general CSS file here -->
-    <link rel="stylesheet" href="css/general.css" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Anton|Francois+One|Oswald|Roboto:700&display=swap" rel="stylesheet">
- 
-  </head>
-
-  <body>
- 
-    <div class="container">
+          <div className="container">
   
-    <header class="row py-4">
+    <header className="row py-4">
     
-      <div class="col-3">
+      <div className="col-3">
         <img src="images/header_footer/the-stand-logo-full-blue.png" alt="the stand logo" width="200" />
       </div>
     
-      <div class="col-6">
+      <div className="col-6">
       </div>
   
-      <div class="col-3">
+      <div className="col-3">
         
-        <div class="row">
-          <div class="col px-0">
+        <div className="row">
+          <div className="col px-0">
             
-            <div class="input-group input-group-sm mb-3">
-              <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2">
+            <div className="input-group input-group-sm mb-3">
+              <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
         
-              <div class="input-group-prepend">
-                 <span class="input-group-text" id="basic-addon2"><img src="images/header_footer/iconfinder_search.png" alt="the search logo" width="24" /></span>
+              <div className="input-group-prepend">
+                 <span className="input-group-text" id="basic-addon2"><img src="images/header_footer/iconfinder_search.png" alt="the search logo" width="24" /></span>
               </div>
             
             </div>
@@ -50,9 +36,9 @@ export default () => (
           </div>
         </div>
 
-        <div class="row">
-          <div class="col text-right px-0">
-            <button type="button" class="btn btn-primary btn-md px-3 px-md-5">Donate</button>
+        <div className="row">
+          <div className="col text-right px-0">
+            <button type="button" className="btn btn-primary btn-md px-3 px-md-5">Donate</button>
           </div>
         </div>
 
@@ -62,83 +48,83 @@ export default () => (
      </header>
 
 
-<div class="row">
-  <div class="col px-0">
+<div className="row">
+  <div className="col px-0">
 
-  <nav class="navbar navbar-expand-lg navbar-light">
+  <nav className="navbar navbar-expand-lg navbar-light">
   
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
     
-      <ul class="navbar-nav mr-auto">
+      <ul className="navbar-nav mr-auto">
       
-        <li class="nav-item dropdown">
+        <li className="nav-item dropdown">
         
-          <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stories</a>
+          <a className="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stories</a>
         
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">News</a>
-            <a class="dropdown-item" href="#">Community</a>
-            <a class="dropdown-item" href="#">Environment</a>
-            <a class="dropdown-item" href="#">Business</a>
-            <a class="dropdown-item" href="#">School & Youth</a>
-            <a class="dropdown-item" href="#">Entertainment</a>
-            <a class="dropdown-item" href="#">Religion & Faith</a>
-            <a class="dropdown-item" href="#">Fatherhood</a>
-            <a class="dropdown-item" href="#">Southside Achiever</a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="#">News</a>
+            <a className="dropdown-item" href="#">Community</a>
+            <a className="dropdown-item" href="#">Environment</a>
+            <a className="dropdown-item" href="#">Business</a>
+            <a className="dropdown-item" href="#">School & Youth</a>
+            <a className="dropdown-item" href="#">Entertainment</a>
+            <a className="dropdown-item" href="#">Religion & Faith</a>
+            <a className="dropdown-item" href="#">Fatherhood</a>
+            <a className="dropdown-item" href="#">Southside Achiever</a>
 
           
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Photo Walk</a>
-            <a class="dropdown-item" href="#">They Wear Blue</a>
-            <a class="dropdown-item" href="#">My Housing Matters</a>
-            <a class="dropdown-item" href="#">From Where We Stand</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="#">Photo Walk</a>
+            <a className="dropdown-item" href="#">They Wear Blue</a>
+            <a className="dropdown-item" href="#">My Housing Matters</a>
+            <a className="dropdown-item" href="#">From Where We Stand</a>
           </div>
 
         </li>
 
-        <li class="nav-item dropdown">
+        <li className="nav-item dropdown">
         
-           <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
+           <a className="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
         
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">History</a>
-              <a class="dropdown-item" href="#">Print Copies</a>
-              <a class="dropdown-item" href="#">Flickr</a>
-              <a class="dropdown-item" href="#">FAQ</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">History</a>
+              <a className="dropdown-item" href="#">Print Copies</a>
+              <a className="dropdown-item" href="#">Flickr</a>
+              <a className="dropdown-item" href="#">FAQ</a>
             </div>
         </li>
 
-        <li class="nav-item dropdown">
+        <li className="nav-item dropdown">
         
-           <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Opportunites</a>
+           <a className="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Opportunites</a>
         
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Job Openings</a>
-              <a class="dropdown-item" href="#">Volunteer</a>
-              <a class="dropdown-item" href="#">Skills Training</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Job Openings</a>
+              <a className="dropdown-item" href="#">Volunteer</a>
+              <a className="dropdown-item" href="#">Skills Training</a>
             </div>
         </li>
 
-        <li class="nav-item dropdown">
+        <li className="nav-item dropdown">
         
-           <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Calendar</a>
+           <a className="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Calendar</a>
         
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Events</a>
-              <a class="dropdown-item" href="#">Announcements</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Events</a>
+              <a className="dropdown-item" href="#">Announcements</a>
             </div>
         </li>
 
-        <li class="nav-item">
-          <a class="nav-link px-3" href="#">Contact<span class="sr-only"></span></a>
+        <li className="nav-item">
+          <a className="nav-link px-3" href="#">Contact<span className="sr-only"></span></a>
         </li>
 
-        <li class="nav-item">
-          <a class="nav-link px-3" href="#">Getting Involved<span class="sr-only"></span></a>
+        <li className="nav-item">
+          <a className="nav-link px-3" href="#">Getting Involved<span className="sr-only"></span></a>
         </li>
       </ul>
 
@@ -148,6 +134,14 @@ export default () => (
   </div>
 </div>
 
+
+  <section>
+      <div class="row mt-2">
+            <div class="col-md-12 so">
+                <h1 class="mainheading">Submit Opportunities</h1>
+              <p class="lead">Submit local jobs, events, and volunteer opportunities, as well as recurring events to The Stand.<br></br> Fill out the form with all information regarding your opportunity. The Stand's team will review all submissions and contact you if any additional questions need be answered.</p>
+            </div>
+</div>    
 
     <div class="row pt-4">
       <div class="col-md-9">
@@ -191,75 +185,73 @@ export default () => (
           </div>
 
     </div>
+  
 
-
-
-  <footer class="row background-blue footer">
+  <footer classNameName="row background-blue footer" />
     
-    <div class="col-md-3 py-4 text-center">
+    <div classNameName="col-md-3 py-4 text-center">
       <img src="images/header_footer/the-stand-logo-white.png" alt="Stand Logo" width="150" />
     </div>
 
-    <div class="col-md-6 py-4 text-center footer-text">
+    <div className="col-md-6 py-4 text-center footer-text">
       
-      <h4 class="pb-0">SUBSCRIBE</h4>
+      <h4 className="pb-0">SUBSCRIBE</h4>
       <p>To recieve a weekly newsletter with popular stories and upcoming events:</p>
 
-      <div class="input-group input-group-sm mb-3 container px-0">
+      <div className="input-group input-group-sm mb-3 container px-0">
         
-        <input type="text" class="form-control" placeholder="Insert Email" aria-label="Insert Email" aria-describedby="basic-addon2">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon2">Submit</span>
+        <input type="text" className="form-control" placeholder="Insert Email" aria-label="Insert Email" aria-describedby="basic-addon2" / >
+        <div className="input-group-prepend" />
+          <span className="input-group-text" id="basic-addon2">Submit</span>
         </div>
+     
       
       </div>
+      
 
-      <ul class="list-inline">
+      <ul className="list-inline">
 
-        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Stories</a></li>
+        <li className="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" className="footer-link">Stories</a></li>
 
-        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">About</a></li>
+        <li className="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" className="footer-link">About</a></li>
 
-        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Contact</a></li>
+        <li className="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" className="footer-link">Contact</a></li>
 
-        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Getting Involved</a></li>
+        <li className="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" className="footer-link">Getting Involved</a></li>
       </ul>
 
-      <ul class="list-inline">
+      <ul className="list-inline">
 
-        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Calendar</a></li>
+        <li className="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" className="footer-link">Calendar</a></li>
 
-        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Opportunities</a></li>
+        <li className="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" className="footer-link">Opportunities</a></li>
 
-        <li class="list-inline-item mx-2"><a target="new" href="https://www.flickr.com/photos/mysouthsidestand/" class="footer-link">Flickr</a></li>
+        <li className="list-inline-item mx-2"><a target="new" href="https://www.flickr.com/photos/mysouthsidestand/" className="footer-link">Flickr</a></li>
 
-        <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Advertising</a></li>
+        <li className="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" className="footer-link">Advertising</a></li>
       </ul>
             
-      <p class="footer-text">&copy; 2019 The South Side Stand. All rights reserved.</p>
+      <p className="footer-text">&copy; 2019 The South Side Stand. All rights reserved.</p>
        
+      </section> </div>
+           
+
+
+    <div className="col-md-3 py-4 text-center">
+
+      <a target="new" href="https://www.facebook.com/TheStandNewspaper/" ><img src="images/header_footer/iconfinder_facebook.png" alt="Facebook Logo" width="36" className="mx-1"/></a>
+          
+      <a target="new" href="http://twitter.com/mysouthside" ><img src="images/header_footer/iconfinder_twitter.png" alt="Twitter Logo" width="36" className="mx-1" /></a>
+          
+      <a target="new" href="INSERT STAND INSTAGRAM LINK" ><img src="images/header_footer/iconfinder_instagram.png" alt="Instagram Logo" width="36" className="mx-1" /></a>
     </div>
 
 
-    <div class="col-md-3 py-4 text-center">
-
-      <a target="new" href="https://www.facebook.com/TheStandNewspaper/" ><img src="images/header_footer/iconfinder_facebook.png" alt="Facebook Logo" width="36" class="mx-1"/></a>
-          
-      <a target="new" href="http://twitter.com/mysouthside" ><img src="images/header_footer/iconfinder_twitter.png" alt="Twitter Logo" width="36" class="mx-1" /></a>
-          
-      <a target="new" href="INSERT STAND INSTAGRAM LINK" ><img src="images/header_footer/iconfinder_instagram.png" alt="Instagram Logo" width="36" class="mx-1" /></a>
-    </div>
-
-</footer>
   
 </div>
   
+    );
+  }
+}
 
-    <!-- Optional JavaScript but required for the cool stuff -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-  </Layout>
-)
+export default Getinvolved; 
