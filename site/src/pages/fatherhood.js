74c4e3a34803de04f9d4532ab7b4499594c4836f
John Dishaw <meraki.official@DESKTOP-L4GNRBN.localdomain>
2020-01-30 19:45:12 -0500

    fatherhood

--- site/src/pages/fatherhood.js
+++ site/src/pages/fatherhood.js
@@ -3,6 +3,10 @@ import Layout from '../components/layout';
 import Navigation from '../components/navigation';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Card from '../components/card';
+import Nav from '../components/nav';
+
+
 
 
 class Fatherhood extends React.Component {
@@ -11,7 +15,7 @@ class Fatherhood extends React.Component {
       <div>
         <Layout />
         <Navigation />
-        <Header />
+
 
         <html lang="en">
           <head>
@@ -34,126 +38,13 @@ class Fatherhood extends React.Component {
 
             <div class="container">
 
-              <header class="row py-4">
-
-                <div class="col-3">
-                  <img src="images/header_footer/the-stand-logo-full-blue.png" alt="the stand logo" width="200" />
-                </div>
-
-                <div class="col-6">
-                </div>
-
-                <div class="col-3">
-
-                  <div class="row">
-                    <div class="col px-0">
-
-                      <div class="input-group input-group-sm mb-3">
-                        <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
-
-                        <div class="input-group-prepend">
-                          <span class="input-group-text" id="basic-addon2"><img src="images/header_footer/iconfinder_search.png" alt="the search logo" width="24" /></span>
-                        </div>
-
-                      </div>
-
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="col text-right px-0">
-                      <button type="button" class="btn btn-primary btn-md px-3 px-md-5">Donate</button>
-                    </div>
-                  </div>
-
-
-                </div>
-
-              </header>
+              <Header />
 
 
               <div class="row">
                 <div class="col px-0">
 
-                  <nav class="navbar navbar-expand-lg navbar-light">
-
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                      <ul class="navbar-nav mr-auto">
-
-                        <li class="nav-item dropdown">
-
-                          <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stories</a>
-
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">News</a>
-                            <a class="dropdown-item" href="#">Community</a>
-                            <a class="dropdown-item" href="#">Environment</a>
-                            <a class="dropdown-item" href="#">Business</a>
-                            <a class="dropdown-item" href="#">School & Youth</a>
-                            <a class="dropdown-item" href="#">Entertainment</a>
-                            <a class="dropdown-item" href="#">Religion & Faith</a>
-                            <a class="dropdown-item" href="#">Fatherhood</a>
-                            <a class="dropdown-item" href="#">Southside Achiever</a>
-
-
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Photo Walk</a>
-                            <a class="dropdown-item" href="#">They Wear Blue</a>
-                            <a class="dropdown-item" href="#">My Housing Matters</a>
-                            <a class="dropdown-item" href="#">From Where We Stand</a>
-                          </div>
-
-                        </li>
-
-                        <li class="nav-item dropdown">
-
-                          <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
-
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">History</a>
-                            <a class="dropdown-item" href="#">Print Copies</a>
-                            <a class="dropdown-item" href="#">Flickr</a>
-                            <a class="dropdown-item" href="#">FAQ</a>
-                          </div>
-                        </li>
-
-                        <li class="nav-item dropdown">
-
-                          <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Opportunites</a>
-
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Job Openings</a>
-                            <a class="dropdown-item" href="#">Volunteer</a>
-                            <a class="dropdown-item" href="#">Skills Training</a>
-                          </div>
-                        </li>
-
-                        <li class="nav-item dropdown">
-
-                          <a class="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Calendar</a>
-
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Events</a>
-                            <a class="dropdown-item" href="#">Announcements</a>
-                          </div>
-                        </li>
-
-                        <li class="nav-item">
-                          <a class="nav-link px-3" href="#">Contact<span class="sr-only"></span></a>
-                        </li>
-
-                        <li class="nav-item">
-                          <a class="nav-link px-3" href="#">Getting Involved<span class="sr-only"></span></a>
-                        </li>
-                      </ul>
-
-                    </div>
-                  </nav>
+                  <Nav />
 
                 </div>
               </div>
@@ -191,58 +82,17 @@ class Fatherhood extends React.Component {
                     <h3 class="mb-4">Fatherhood</h3>
 
                     <div class="row">
+                      <Card name="Chol Majok" nominated="David Chaplin" published="December 3, 2019" />
 
-                      <div class="col-md-4 pb-3">
-                        <a href="#"><img src="images/fatherhood/Chol Majok.jpg" alt="Chol Majok" class="img-fluid pb-2" /></a>
-                        <p class="section-header mb-2">Q&A with Father</p>
-                        <h2 class="my-2">Chol Majok</h2>
-                        <p class="mb-0">Nominated by David Chaplin</p>
-                        <p class="publication-date">Published on December 3, 2019</p>
-                      </div>
-
-                      <div class="col-md-4 pb-3">
-                        <a href="#"><img src="images/fatherhood/Hasan Stephens.jpg" alt="Hasan Stephens" class="img-fluid pb-2" /></a>
-                        <p class="section-header mb-2">Q&A with Father</p>
-                        <h2 class="my-2">Hasan Stephens</h2>
-                        <p class="mb-0">Nominated by Tedd Perry</p>
-                        <p class="publication-date">Published on October 30, 2019</p>
-                      </div>
-
-                      <div class="col-md-4 pb-3">
-                        <a href="#"><img src="images/fatherhood/David Chaplin.jpg" alt="David Chaplin" class="img-fluid pb-2" /></a>
-                        <p class="section-header mb-2">Q&A with Father</p>
-                        <h2 class="my-2">David Chaplin</h2>
-                        <p class="mb-0">Nominated by Twiggy Billue</p>
-                        <p class="publication-date">Published on August 28, 2019</p>
-                      </div>
+                      <Card name="Hasan Stephens" nominated="Tedd Perry" published="October 30, 2019" />
+                      <Card name="David Chaplan" nominated="Twiggy Billue" published="August 28, 2019" />
 
                     </div>
 
                     <div class="row">
-
-                      <div class="col-md-4 pb-3">
-                        <a href="#"><img src="images/fatherhood/Yusuf Abdul-Qadir.jpg" alt="Yusuf Abdul-Qadir" class="img-fluid pb-2" /></a>
-                        <p class="section-header mb-2">Q&A with Father</p>
-                        <h2 class="my-2">Yusuf Abdul-Qadir</h2>
-                        <p class="mb-0">Nominated by Twiggy Billue</p>
-                        <p class="publication-date">Published on May 21, 2019</p>
-                      </div>
-
-                      <div class="col-md-4 pb-3">
-                        <a href="#"><img src="images/fatherhood/Tyrone Dixon.jpg" alt="Tyrone Dixon" class="img-fluid pb-2" /></a>
-                        <p class="section-header mb-2">Q&A with Father</p>
-                        <h2 class="my-2">Tyrone Dixon</h2>
-                        <p class="mb-0">Nominated by Rachielle Scrivens</p>
-                        <p class="publication-date">Published on April 2, 2019</p>
-                      </div>
-
-                      <div class="col-md-4 pb-3">
-                        <a href="#"><img src="images/fatherhood/Rashawn Sullivan.jpg" alt="Rashawn Sullivan" class="img-fluid pb-2" /></a>
-                        <p class="section-header mb-2">Q&A with Father</p>
-                        <h2 class="my-2">Rashawn Sullivan</h2>
-                        <p class="mb-0">Uncle serves as a mentor to his niece and nephew</p>
-                        <p class="publication-date">Published on March 4, 2019</p>
-                      </div>
+                      <Card name="Yusuf Abdul-Qadir" nominated="Twiggy Billue" published="May 21, 2019" />
+                      <Card name="Tyrone Dixo" nominated="Rachielle Scrivens" published="April 2, 2019" />
+                      <Card name="Rashawn Sullivan" nominated="niece and nephew" published="March 4, 2019" />
 
                     </div>
 
@@ -255,64 +105,8 @@ class Fatherhood extends React.Component {
                 </div>
               </section>
 
+              <Footer />
 
-              <footer class="row background-blue footer">
-
-                <div class="col-md-3 py-4 text-center">
-                  <img src="images/header_footer/the-stand-logo-white.png" alt="Stand Logo" width="150" />
-                </div>
-
-                <div class="col-md-6 py-4 text-center footer-text">
-
-                  <h4 class="pb-0">SUBSCRIBE</h4>
-                  <p>To recieve a weekly newsletter with popular stories and upcoming events:</p>
-
-                  <div class="input-group input-group-sm mb-3 container px-0">
-
-                    <input type="text" class="form-control" placeholder="Insert Email" aria-label="Insert Email" aria-describedby="basic-addon2" />
-                    <div class="input-group-prepend">
-                      <span class="input-group-text" id="basic-addon2">Submit</span>
-                    </div>
-
-                  </div>
-
-                  <ul class="list-inline">
-
-                    <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Stories</a></li>
-
-                    <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">About</a></li>
-
-                    <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Contact</a></li>
-
-                    <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Getting Involved</a></li>
-                  </ul>
-
-                  <ul class="list-inline">
-
-                    <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Calendar</a></li>
-
-                    <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Opportunities</a></li>
-
-                    <li class="list-inline-item mx-2"><a target="new" href="https://www.flickr.com/photos/mysouthsidestand/" class="footer-link">Flickr</a></li>
-
-                    <li class="list-inline-item mx-2"><a target="new" href="INSERT LINK HERE" class="footer-link">Advertising</a></li>
-                  </ul>
-
-                  <p class="footer-text">&copy; 2019 The South Side Stand. All rights reserved.</p>
-
-                </div>
-
-
-                <div class="col-md-3 py-4 text-center">
-
-                  <a target="new" href="https://www.facebook.com/TheStandNewspaper/" ><img src="images/header_footer/iconfinder_facebook.png" alt="Facebook Logo" width="36" class="mx-1" /></a>
-
-                  <a target="new" href="http://twitter.com/mysouthside" ><img src="images/header_footer/iconfinder_twitter.png" alt="Twitter Logo" width="36" class="mx-1" /></a>
-
-                  <a target="new" href="INSERT STAND INSTAGRAM LINK" ><img src="images/header_footer/iconfinder_instagram.png" alt="Instagram Logo" width="36" class="mx-1" /></a>
-                </div>
-
-              </footer>
 
             </div>
 
@@ -326,7 +120,7 @@ class Fatherhood extends React.Component {
         </html>
 
 
-        <Footer />
+
       </div>
     )
   }
