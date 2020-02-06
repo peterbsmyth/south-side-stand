c9be363e284fc3e6c3b38a6d9e10da1573e15379
John Dishaw <meraki.official@DESKTOP-L4GNRBN.localdomain>
2020-02-06 18:59:57 -0500

    head/body removal

--- site/src/pages/adpage.js
+++ site/src/pages/adpage.js
@@ -7,24 +7,6 @@ export default () => (
   <Layout>
     <h1>This is the ad page</h1>
 
-
-    <head>
-
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-      <title>Advertising</title>
-
-
-      <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
-
-
-      <link rel="stylesheet" href="css/general.css" type="text/css" />
-      <link href="https://fonts.googleapis.com/css?family=Anton|Francois+One|Oswald|Roboto:700&display=swap" rel="stylesheet" />
-
-    </head>
-
-    <body>
       <div class="container">
 
         <Header />
@@ -123,7 +105,6 @@ export default () => (
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
       <script src="js/bootstrap.min.js"></script>
-    </body>
 
   </Layout>
 )
