c9e3d4bf7852d30538e3a7e7536c1cd0fd2b7a8f
Peter Smith <peter@upstateinteractive.io>
2020-02-15 11:13:21 -0500

    refactor: remove unused navigation

--- site/src/components/layout.js
+++ site/src/components/layout.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navigation from './navigation'
 import './layout.css'
 const layout = ({ children }) => {
   return (
     <>
-      <Navigation />
       <main>{children}</main>
     </>
   )
